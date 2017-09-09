31d76107a522d1c639cb06a2c9053bbe21d7f652
maimai84 <maialmasalmah@gmail.com>
2017-09-09 08:50:57 +0300

    easyBackbone

--- backbone-pond/fishModel.js
+++ backbone-pond/fishModel.js
@@ -7,6 +7,11 @@ var Fish = Backbone.Model.extend({
     image: 'http://www.google.com',
     description: 'Regular old fish',
     displayInfo: false
+  },
+  toggleDescription:function(){
+  	this.Model.on('click',function(){this.set(!this.get('displayInfo'))}
+  		,this)
+  	this.render();
   }
 
 });
